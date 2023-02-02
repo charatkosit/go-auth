af315e68e43389ee150a2a494705028f24ea74f7
charat <charatkosit@gmail.com>
2023-02-02 11:57:59 +0700

    add environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+export const environment = {
+  production: false,
+  dbUrl: "127.0.0.1",
+  port: 3308,
+  dbUsername: "root",
+  dbPassword: "1234",
+  dbName: "testhaha",
+  synchronize: true,
+  sapUrl: "http://192.168.20.17:8880/apigoplus/GetBillTo/",
+  sapApiToken: "z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn",
+  jwt_secrect: "!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-Ka",
+  baseUrl: "",
+  project: "Build Dev:v0.1.4b on 19/11/22",
+  release: "%release%",
+};
